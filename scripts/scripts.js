368c2d163f26fdab4f2fe1fcaacfa7399f1fc439
MDOB93 <maik.dobosch@posteo.de>
2026-02-04 17:21:59 +0100

    feat: update scripts.js main.css | add function to render tags in cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,37 +8,74 @@ async function fetchData(){
 
     console.log(responseAsJson);
 
-    const allPokemon = [];
+    const pokemonURL = [];
 
     // aweit only in async funtions (const pokemon = responseAsJson.results) | if you want to read the files in sequence, you cannot use forEach indeed. Just use a modern for … of loop instead, in which await will work as expected.
     // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
     for (const pokemon of responseAsJson.results) {
         const urlRef = await fetch(pokemon.url);
         const urlData = await urlRef.json();
-        allPokemon.push(urlData);
+        pokemonURL.push(urlData);
     }
 
-    console.log(allPokemon);
+    console.log(pokemonURL);
 
-    renderPokemon(allPokemon)
+    renderPokemon(pokemonURL)
 };
 
-function renderPokemon(allPokemon){
+function renderPokemon(pokemonURL){
     const contentRef = document.getElementById('content');
 
-    allPokemon.forEach((arrayElement) => {
-        console.log(`ID: ${arrayElement.id} | Name: ${arrayElement.name} | Img URL: ${arrayElement.sprites.front_shiny}`);
+    pokemonURL.forEach((allPokemons) => {
+        console.log(`ID: ${allPokemons.id} | Name: ${allPokemons.name} | Img URL: ${allPokemons.sprites.other.dream_world.front_default}`);
+        
+        console.log(pokemonNameUpCase(allPokemons));
+        
+        let tagArray = getPokemonTags(allPokemons)
+        console.log(tagArray);
         
         contentRef.innerHTML += `
             <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                <div id="pokemonId" class="card-header">#${arrayElement.id}</div>
+                <div id="pokemonId" class="card-header">#${allPokemons.id}</div>
                 <div class="card-body">
-                    <h5 id="pokemonName" class="card-title">${arrayElement.name}</h5>
+                    <h5 id="pokemonName" class="card-title">${pokemonNameUpCase(allPokemons)}</h5>
                     <div class="pokemonImgWraper">
-                        <img id="pokemonImg" src="${arrayElement.sprites.front_shiny}" alt="Pokemon Img Front">
-                        <p id="pokemonTag" class="card-text"></p>
+                        <img id="pokemonImg" src="${allPokemons.sprites.other.dream_world.front_default}" alt="Pokemon Img Front">
+                        <div>${getTagHtml(tagArray)}</div>
                     </div>
                 </div>
-            </div>`;
+            </div>`
+        ;
     })
+};
+
+function pokemonNameUpCase(allPokemons) {
+    const PokemonName = allPokemons.name
+    // cahrAt(0) get the first letter of PokemonName than make it toUpperCase()
+    // PokemonName.slice(1) get the rest of the word
+    const nameUpCase = PokemonName.charAt(0).toUpperCase() + PokemonName.slice(1);
+    
+    return nameUpCase;
+};
+
+function getPokemonTags(allPokemons) {
+    let pokemonTypes = allPokemons.types
+    let pokemonTags = [];
+
+    // iterate through pokemonTypes to get type.name of pokemon
+    pokemonTypes.forEach ((pokemonTagsArray) => {
+        pokemonTags.push(pokemonTagsArray.type.name)
+    })
+
+    return pokemonTags;
+};
+
+function getTagHtml(pokemonTagsArray) {
+    let typeHtml = "";
+    // iterate through pokemonTagsArray and save it in html as <span>
+    pokemonTagsArray.forEach((tag) => {
+        typeHtml += `<span>${tag} </span>`;
+    })
+
+    return typeHtml;
 };
